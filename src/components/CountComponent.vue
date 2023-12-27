447cceb703db4396e97258128c67ee3aa3c3d0ae
SylvinIsamaza <isamazasylvin@gmail.com>
2023-12-27 07:51:02 +0200

    styling homepage

--- src/components/CountComponent.vue
+++ src/components/CountComponent.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="px-[40px] flex flex-col py-2 gap-4" >
-
-    <div class="p-[16px] pt-0 pl-0 w-full py-1 bg-tertiary rounded-lg flex flex-col gap-">
+  <div class=" bg-tertiary  flex flex-col py-2 gap-4" >
+    <div class="bg-[#1A2C38] flex flex-col gap-4 px-[40px] ">
+      <div class="p-[16px] pt-0 pl-0 w-full py-1 bg-tertiary rounded-lg flex flex-col gap-">
       <div class="flex pt-0 w-full">
         <div class="flex-col   px-2 rounded-t-lg rounded-r-none bg-secondary py-3 min-w-fit">
           <div class="bg-tertiary flex w rounded-full p-[3px]">
@@ -58,7 +58,7 @@
               1.60x
             </div>
           </div>
-          <div ref="pixiContainer" class="h-[200px] flex items-center justify-center">
+          <div ref="pixiContainer" class="h-[200px] bg-[#0F212E] flex items-center justify-center">
             <p class="text-5xl font-[600] text-white">1.01x</p>
           </div>
           <div class="flex items-center py-2 gap-3 justify-between w-full px-[16px] rounded-md  bg-primary">
@@ -180,13 +180,155 @@
 
       </tbody>
     </table>
+    </div>
+ <div class="flex gap-3 px-[30px] flex-col">
+  <div class="grid grid-cols-6 gap-[51px]">
+      <div class="flex-col flex items-start">
+        <p class="font-[600] py-2 text-white text-[14px]">
+          体育
+        </p>
+        <p class="font-[600] text-[#B1BAD3] text-[14px]">
+          主页
+        </p>
+        <p class="font-[600] text-[#B1BAD3] text-[14px]">
+          滚球
+        </p>
+        <p class="font-[600] text-[#B1BAD3] text-[14px]">
+         
+          体育博彩规则
+        </p>
+      </div>
+      <div class="flex-col flex items-start">
+        <p class="font-[600] py-2 text-white text-[14px]">
+          娱乐城
+        </p>
+        <p class="font-[600] text-[#B1BAD3] text-[14px]">
+          游戏
+        </p>
+        <p class="font-[600] text-[#B1BAD3] text-[14px]">
+          VIP俱乐部
+        </p>
+        <p class="font-[600] text-[#B1BAD3] text-[14px]">
+         
+          促销活动
+        </p>
+      </div>
+      <div class="flex-col flex items-start">
+        <p class="font-[600] py-2 text-white text-[14px]">
+          支持
+        </p>
+        <p class="font-[600] text-[#B1BAD3] text-[14px]">
+          公平性
+        </p>
+        <p class="font-[600] text-[#B1BAD3] text-[14px]">
+          联盟计划
+        </p>
+        <p class="font-[600] text-[#B1BAD3] text-[14px]">
+         
+          负责任博彩
+        </p>
+        <p class="font-[600] text-[#B1BAD3] text-[14px]">
+         
+          在线支持
+       </p>
+       <p class="font-[600] flex items-center gap-2 text-[#B1BAD3] text-[14px]">
+         
+        帮助中心 <img src="../assets/icons/open.svg" alt="">
+       </p>
+      </div>
+      <div class="flex-col flex items-start">
+        <p class="font-[600] py-2 text-white text-[14px]">
+          支持
+        </p>
+        <p class="font-[600] text-[#B1BAD3] text-[14px]">
+          公平性
+        </p>
+        <p class="font-[600] text-[#B1BAD3] text-[14px]">
+          联盟计划
+        </p>
+        <p class="font-[600] text-[#B1BAD3] text-[14px]">
+         
+          负责任博彩
+        </p>
+        <p class="font-[600] text-[#B1BAD3] text-[14px]">
+         
+          在线支持
+       </p>
+       <p class="font-[600] flex items-center gap-2 text-[#B1BAD3] text-[14px]">
+         
+        帮助中心
+       </p>
+      </div>
+     
+      <div class="flex-col flex items-start">
+        <p class="font-[600] text-white py-2 text-[14px]">
+          支持
+        </p>
+        <p class="font-[600] text-[#B1BAD3] text-[14px]">
+          公平性
+        </p>
+        <p class="font-[600] text-[#B1BAD3] text-[14px]">
+          联盟计划
+        </p>
+        <p class="font-[600] text-[#B1BAD3] text-[14px]">
+         
+          负责任博彩
+        </p>
+        <p class="font-[600] text-[#B1BAD3] text-[14px]">
+         
+          在线支持
+       </p>
+       <p class="font-[600] flex items-center gap-2 text-[#B1BAD3] text-[14px]">
+         
+        帮助中心
+       </p>
+      </div>
+     
+     
+      <div class="flex-col items-start flex justify-center">
+        <p class="font-[600] text-white py-2 text-[14px]">
+        语言
+          
+        </p>
+     
+      <p class="font-[600] flex items-center gap-2 text-white text-[14px]">
+        
+       帮助中心 <img src="../assets/icons/down_arrow.svg" alt="">
+      </p>
+      <p class="font-[600] flex items-center gap-2 text-white text-[14px]">
+        
+        赔率
+       </p>
+       <p class="font-[600] flex items-center gap-2 text-white text-[14px]">
+        
+        小数式 <img src="../assets/icons/down_arrow.svg" alt="">
+       </p>
+     </div>
+    </div>
+    <CurrencyComponent></CurrencyComponent>
+    <div class="flex w-full justify-between items-center">
+<div class="flex items-center w-full justify-between">
+  <img src="../assets/icons/verify.png" alt="" class="h-[80px]">
+  <img src="../assets/icons/alfa-logo.svg" alt="" class="h-[60px]">
+  <img src="../assets/icons/everton-logo.svg" alt="" class="h-[60px]">
+  <img src="../assets/icons/ufc-partner.svg" alt="" class="h-[60px]">
+</div>
   </div>
+ </div>
+    
+ 
+   
+  </div>
+
 </template>
 
 <script>
 import * as PIXI from 'pixi.js';
-
+import CurrencyComponent from './Currency.vue';
 export default {
+  components:{
+CurrencyComponent
+  },
   data() {
     return {
       app:null,
---
+++ src/components/Currency.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="flex w-full items-center justify-between">
+  <img src="../assets/icons/litecoin.svg" alt="" class="h-[40px]">
+  <img src="../assets/icons/bitcoinicon.svg" alt="" class="h-[40px]">
+  <img src="../assets/icons/ethereum.svg" alt="" class="h-[40px]">
+  <img src="../assets/icons/tron.svg" alt="" class="h-[40px]">
+  <img src="../assets/icons/ripple.svg" alt="" class="h-[40px]">
+  
+  <img src="../assets/icons/bitcoin-cash.svg" alt="" class="h-[40px]">
+  
+ 
+</div>
+<div class="flex items-center justify-between">
+  <img src="../assets/icons/tether.svg" alt="" class="h-[40px]">
+  <img src="../assets/icons/safe-gamble.svg" alt="" class="h-[40px]">
+  <img src="../assets/icons/18plus.svg" alt="" class="h-[40px]">
+</div>
+</template>
+
+<script>
+export default{
+  name:"CurrencyComponent"
+}
+
+</script>
